4956cc34ab32bdc54bb7fd78a6c43013513fe58b
Chimatara Kalu <taradaniel05@gmail.com>
2024-01-03 22:45:23 +0100

    Level 2 Answers

--- day1/script.js
+++ day1/script.js
@@ -82,3 +82,71 @@ console.log(firstText.concat(` ${secondText}`))
 
 // 25. Use repeat() method to print 30 Days Of JavaScript 2 times
 console.log(challenge.repeat(2))
+
+
+// LEVEL 2
+
+// 1. Using console.log() print out the following statement:
+console.log('\" There is no exercise better for the heart than reaching down and lifting people up.\" \n by John Holmes teaches us to help one another.')
+
+// 2. Using console.log() print out the following quote by Mother Teresa:
+console.log('\"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead.\"')
+
+// 3. Check if typeof '10' is exactly equal to 10. If not make it exactly equal
+let number = '10';
+let num = 10;
+if(typeof number === num){
+    console.log('It is equal')
+} else{
+    let numInt = parseInt(number)
+    console.log(`${num} is equal to ${numInt}`)
+}
+
+// 4. Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10
+if(parseFloat(9.8) == num){
+    console.log('true')
+} else{
+    console.log(Math.round(9.8))
+}
+
+// 5. Check if 'on' is found in both python and jargon
+let python = 'python'
+let jargon = 'jargon'
+console.log(python.includes('on') && jargon.includes('on'))
+
+// 6. I hope this course is not full of jargon. Check if jargon is in the sentence.
+let p = 'I hope this course is not full of jargon';
+console.log(p.includes('jargon'))
+
+// 7. Generate a random number between 0 and 100 inclusively
+const randomNumber = Math.floor(Math.random() * 101)
+console.log(randomNumber)
+
+// 8. Generate a random number between 50 and 100 inclusively
+const anotherRandomNumber = Math.floor(Math.random() * (100 - 50 + 1) + 50)
+console.log(anotherRandomNumber)
+
+// 9. Generate a random number between 0 and 255 inclusively.
+const randomNum = Math.floor(Math.random() * 256)
+console.log(randomNum)
+
+// 10. Access the 'JavaScript' string characters using a random number.
+let JavaScriptString = 'JavaScript'
+let randomIndex = Math.floor(Math.random() * JavaScriptString.length)
+let randomCharacter = JavaScriptString.charAt(randomIndex)
+console.log(randomCharacter) 
+
+// 11. Use console.log() and escape characters to print the following pattern.
+// 1 1 1 1 1
+// 2 1 2 4 8
+// 3 1 3 9 27
+// 4 1 4 16 64
+// 5 1 5 25 125
+console.log('1\t1\t1\t1\t1\t \n 2\t1\t2\t4\t8\t \n 3\t1\t3\t9\t27\t \n 4\t1\t4\t16\t64\t \n 5\t1\t5\t25\t125\t') 
+
+// 12. Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction
+let statement = 'You cannot end a sentence with because because because is a conjunction'
+console.log(statement.indexOf('because'))
+console.log(statement.lastIndexOf('because'))
+console.log(statement.substring(31, 54))
+
