e5b249fffe1dbf6157c90ffaacb47ce0c4b9b269
Chimatara Kalu <taradaniel05@gmail.com>
2024-01-05 23:39:04 +0100

    day3

--- day1&2/script.js
+++ day1&2/script.js
@@ -170,4 +170,9 @@ console.log(` The number of times the word \'because\' appeared is ${becauseCoun
 //     const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
  const regexSentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
  let cleanStatement = regexSentence.replace(/[^\w\s]/g, '')
- console.log(cleanStatement)
+//  console.log(cleanStatement)
+
+// 4. Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+const stat = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+const extractNum = stat.match(/\d+/g)
+console.log(extractNum)
---
+++ day3/script.js
@@ -0,0 +1,20 @@
+// Excercises: Level 1
+
+// Questions
+
+// 1. Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types
+let firstName = 'Chimatara';
+let lastName = 'Kalu';
+let country = 'Nigeria';
+let city = 'Lagos';
+let age = 25;
+let isMarried = false;
+let year = 1998;
+
+console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof country)
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof isMarried)
+console.log(typeof year)
