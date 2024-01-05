ae7c06d4b58c3e8e1298aa213941ab75b785f6b2
Chimatara Kalu <taradaniel05@gmail.com>
2024-01-05 23:56:08 +0100

    day3

--- day3/script.js
+++ day3/script.js
@@ -33,3 +33,42 @@ let numInt = parseInt(numFloat)
 console.log(numInt == number)
 
 //4. Boolean value is either true or false.
+// i. Write three JavaScript statement which provide truthy value.
+// ii. Write three JavaScript statement which provide falsy value.
+
+// Truthy Values
+let myName = 'Tara';
+let hobbies = ['Baking', 'singing', 'coding'];
+let numberOfKnownLanguages = 3;
+
+// Fasly Values
+let isEngaged;
+let drinks = null;
+let canDance = '';
+
+// 5. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+// 4 > 3 true
+// 4 >= 3 false
+// 4 < 3 false
+// 4 <= 3 false
+// 4 == 4 true
+// 4 === 4 true
+// 4 != 4 false
+// 4 !== 4 false
+// 4 != '4' false
+// 4 == '4' true
+// 4 === '4' false
+// Find the length of python and jargon and make a falsy comparison statement.
+
+console.log(4 > 3)
+console.log(4 >= 3)
+console.log(4 < 3)
+console.log(4 <= 3)
+console.log(4 == 4)
+console.log(4 === 4)
+console.log(4 != 4)
+console.log(4 !== 4)
+console.log(4 != '4')
+console.log(4 == '4')
+console.log(4 === '4')
+// Find the length of python and jargon and make a falsy comparison statement.
