383d9992069af23ddb72f43fc90e86bebd017905
Chimatara Kalu <taradaniel05@gmail.com>
2024-01-05 23:44:57 +0100

    day3

--- day3/script.js
+++ day3/script.js
@@ -18,3 +18,18 @@ console.log(typeof city)
 console.log(typeof age)
 console.log(typeof isMarried)
 console.log(typeof year)
+
+
+//2. Check if type of '10' is equal to 10
+let numString = '10';
+let num = 10;
+console.log(typeof numString == num)
+
+
+//3. Check if parseInt('9.8') is equal to 10
+let number = 10;
+let numFloat = 9.8;
+let numInt = parseInt(numFloat)
+console.log(numInt == number)
+
+//4. Boolean value is either true or false.
