d860ba6fc7baee701c28bd1f985285097394c6b6
Chimatara Kalu <taradaniel05@gmail.com>
2024-01-07 14:29:38 +0100

    day3

--- day3/script.js
+++ day3/script.js
@@ -124,7 +124,31 @@ console.log(date.getTime())
 // Level 2
 
 // 1. Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+// Enter base: 20
+// Enter height: 10
+// The area of the triangle is 100
 let triangleBase = prompt('Enter the triangle base', 0)
 let height = prompt('Enter the triangle height', 0)
+console.log('Area of triangle is ' + 0.5 * triangleBase * height)
 
-console.log(0.5 * triangleBase * height)
+// 2. Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c
+// Enter side a: 5
+// Enter side b: 4
+// Enter side c: 3
+// The perimeter of the triangle is 12
+
+let sideA = prompt('Enter Side A', 0)
+let sideB = prompt('Enter Side B', 0)
+let sideC= prompt('Enter Side C', 0)
+console.log('Theperimeter of a triangle is ' + sideA + sideB + sideC);
+
+
+// 3. Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width)
+let rectangleLength = prompt('Enter rectangle length ', 0);
+let rectangleWidth= prompt('Enter rectangle width', 0);
+let rectangleArea = rectangleLength * rectangleWidth;
+let rectanglePerimeter = 2 * (rectangleLength + rectangleWidth);
+console.log(rectangleArea, rectanglePerimeter)
+
+
+// 4. 
