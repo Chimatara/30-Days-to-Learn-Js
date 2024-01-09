c5e61e75e665c94d576db6193036d86eddc0a0fc
Chimatara Kalu <taradaniel05@gmail.com>
2024-01-09 23:30:52 +0100

    day4

--- day4/script.js
+++ day4/script.js
@@ -48,3 +48,40 @@ if(checkEvennumber % 0){
     console.log(`${checkEvennumber} is an odd number`)
 
 }
+
+
+
+// LEVEL 2
+// Write a code which can give grades to students according to theirs scores:
+// 80-100, A
+// 70-89, B
+// 60-69, C
+// 50-59, D
+// 0-49, F
+let scoores;
+if(scoores >= 80){
+    console.log('A')
+}else if(scoores >= 70 && scoores <= 79){
+    console.log('B')
+}else if(scoores >= 60 && scoores <= 69){
+    console.log('D')
+}else if(scoores >= 50 && scoores <= 59){
+    console.log('E')
+}else{
+    console.log('F')
+
+}
+
+
+// 2. Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+// September, October or November, the season is Autumn.
+// December, January or February, the season is Winter.
+// March, April or May, the season is Spring
+// June, July or August, the season is Summer
+
+let date = new Date().getMonth()
+
+let autumn = ['September', 'October', 'November']
+let winter = ['December', 'January', 'February']
+let spring = ['March', 'April', 'May']
+let summer = ['July', 'Auguest', 'June']
