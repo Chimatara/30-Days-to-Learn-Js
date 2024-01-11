9ee065e437a60b6177e43be561a84e5548cb3fa0
Chimatara Kalu <taradaniel05@gmail.com>
2024-01-11 21:52:56 +0100

    day5

--- day5/script.js
+++ day5/script.js
@@ -1,4 +1,22 @@
-const numbers = [1,2,3,4,5]
+// EXCERCISE: LEVEL 1
+// 1. Declare an empty array;
+const emptyArray = [];
 
-  console.log(numbers.slice()) 
-  console.log(numbers.slice(0)) 
+//2. Declare an array with more than 5 number of elements
+const fruits = ['Kiwi', 'watermelon', 'avocado', 'pear', 'grapes', 'apple']
+
+//3. Find the length of your array
+console.log(fruits.length)
+
+//4. Get the first item, the middle item and the last item of the array
+let firstItem = fruits[0];
+let middleItem = fruits.length / 2
+let lastItem = fruits.length - 1
+console.log(firstItem, middleItem, lastItem) 
+
+
+//5. Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+const mixedDataTypes = [{name: 'Tara', age: 25}, 'Plantain', 'playing', 7, true, [1, 2, 3, 4], '']
+
+// 6. Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+
