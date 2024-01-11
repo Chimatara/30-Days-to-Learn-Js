ac5cc5f58224a4d30eeef1e308e57b6394995508
Chimatara Kalu <taradaniel05@gmail.com>
2024-01-11 22:01:22 +0100

    day5

--- day5/script.js
+++ day5/script.js
@@ -10,8 +10,8 @@ console.log(fruits.length)
 
 //4. Get the first item, the middle item and the last item of the array
 let firstItem = fruits[0];
-let middleItem = fruits.length / 2
-let lastItem = fruits.length - 1
+let middleItem = fruits.length / 2 //come back to this
+let lastItem = fruits[fruits.length - 1]
 console.log(firstItem, middleItem, lastItem) 
 
 
@@ -19,4 +19,29 @@ console.log(firstItem, middleItem, lastItem)
 const mixedDataTypes = [{name: 'Tara', age: 25}, 'Plantain', 'playing', 7, true, [1, 2, 3, 4], '']
 
 // 6. Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+// 7. Print the array using console.log()
+console.log(itCompanies)
+
+// 8. Print the number of companies in the array
+console.log(itCompanies.length)
+
+// 9. Print the first company, middle and last company
+let firstCompany = itCompanies[0];
+let middleCompany = itCompanies[itCompanies.length / 2]
+let lastCompany = itCompanies[itCompanies.length - 1]
+console.log(firstCompany, middleCompany, lastCompany) 
+
+// Print out each company
+console.log(itCompanies.length)
+console.log(itCompanies[0])
+console.log(itCompanies[1])
+console.log(itCompanies[2])
+console.log(itCompanies[3])
+console.log(itCompanies[4])
+console.log(itCompanies[5])
+console.log(itCompanies[6])
+
+// Change each company name to uppercase one by one and print them out
 
