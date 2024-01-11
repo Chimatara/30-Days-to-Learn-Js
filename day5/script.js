26e3233f23856d98cc8fd98c532f2def59be2dc6
Chimatara Kalu <taradaniel05@gmail.com>
2024-01-11 23:23:32 +0100

    day5

--- day5/script.js
+++ day5/script.js
@@ -1,3 +1,5 @@
+
+
 // EXCERCISE: LEVEL 1
 // 1. Declare an empty array;
 const emptyArray = [];
@@ -91,3 +93,16 @@ itCompanies.pop()
 console.log(itCompanies)
 
 //23 Remove all IT companies
+
+
+
+// LEVEL 2
+//1 Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
+
+
+// 2. First remove all the punctuations and change the string to array and count the number of words in the array
+let text =
+'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+const words = text.split()
+console.log(words)
+console.log(words.length)
