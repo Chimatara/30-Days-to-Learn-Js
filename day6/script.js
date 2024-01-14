742531217de8643617fb745b770e4f582beca37a
Chimatara Kalu <taradaniel05@gmail.com>
2024-01-14 23:02:20 +0100

    day6

---
+++ day6/script.js
@@ -0,0 +1,20 @@
+// LEVEL 1
+
+// 1. Iterate 0 to 10 using for loop, do the same using while and do while loop
+// /FOR LOOP
+for(let i = 0; i <= 10; i++){
+    console.log(i)
+}
+// WHILE LOOP
+let i = 0
+while(i <= 10){
+    console.log(i)
+    i++  
+}
+// DO WHILE LOOP
+let index = 0
+do{
+    // console.log(index)
+    i++
+    break
+}while (i <= 10)
