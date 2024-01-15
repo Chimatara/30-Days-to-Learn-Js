5409f041ca65518fc87295123e082ee056878e8a
Chimatara Kalu <taradaniel05@gmail.com>
2024-01-15 21:35:06 +0100

    day6

--- day6/script.js
+++ day6/script.js
@@ -68,10 +68,67 @@ for (let i = 0; i < 7; i++) {
 }
 
 
-// // 5. Use for loop to iterate from 0 to 100 and print only even numbers
+// 6 Use loop to print the following pattern:
+// 0 x 0 = 0
+// 1 x 1 = 1
+// 2 x 2 = 4
+// 3 x 3 = 9
+// 4 x 4 = 16
+// 5 x 5 = 25
+// 6 x 6 = 36
+// 7 x 7 = 49
+// 8 x 8 = 64
+// 9 x 9 = 81
+// 10 x 10 = 100
+for(let i = 0; i <= 10; i++){
+    let sum = `${i} x ${i}`
+    console.log(`${sum} = ${i * i}`)
+}
+
+
+// 7. Using loop print the following pattern
+// i    i^2   i^3
+// 0    0     0
+// 1    1     1
+// 2    4     8
+// 3    9     27
+// 4    16    64
+// 5    25    125
+// 6    36    216
+// 7    49    343
+// 8    64    512
+// 9    81    729
+// 10   100   1000
+
+for(let i = 0; i <= 10; i++){
+    let exponentialTwo = i ** 2;
+    let exponentialThree = i ** 3;
+    let combine = `${exponentialTwo} ${exponentialThree}`
+    console.log(`${i} ${combine}`)
+}
+
+
+//7. Use for loop to iterate from 0 to 100 and print only even numbers.
 for(let i = 0; i <= 100; i++){
     if(i % 2 === 0){
         console.log(`The even numbers are ${i}`)
     }
 }
+
+
+// 8. Use for loop to iterate from 0 to 100 and print only odd numbers
+for(let i = 0; i <= 100; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+        console.log(`The odd numbers are ${i}`) //check back
+    }
+}
+
+// 9. Use for loop to iterate from 0 to 100 and print only prime numbers
+// 10. Use for loop to iterate from 0 to 100 and print the sum of all numbers
+let sum = 0
+for(let i = 0; i <= 100; i++){
+  sum = sum + i
+  console.log(`The sum of the loop is ${sum}`)
+}
+
 
