d887796f9633814fc3fa304b110ecfe31810bd59
Chimatara Kalu <taradaniel05@gmail.com>
2024-01-18 23:38:29 +0100

    day6

--- day6/script.js
+++ day6/script.js
@@ -286,3 +286,16 @@ const webTechs = [
 
 //   Use the webTechs array to create the following array of arrays
 // [["HTML", 4], ["CSS", 3],["JavaScript", 10],["React", 5],["Redux", 5],["Node", 4],["MongoDB", 7]]
+let arrayOfArrays = []
+for(let stack of webTechs){
+    arrayOfArrays.push([stack, stack.length])
+}
+console.log(arrayOfArrays)
+
+
+const mernStack = ['MongoDB', 'Express', 'React', 'Node'];
+let mernAcronym = '';
+for (let i = 0; i < mernStack.length; i++) {
+  mernAcronym += mernStack[i].charAt(0);
+}
+console.log('MERN Acronym:', mernAcronym);
